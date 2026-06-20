38d8f519a8d84e32db8104f1140832a1910808cf
Habiba Abdelgowad <158005569+Habiba-Adel@users.noreply.github.com>
2026-06-20 23:33:36 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error);
-    process.exit(1);
   }
 };
 
