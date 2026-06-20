9750e2ced99194b7592b8f11066715348adc7bed
Habiba Abdelgowad <158005569+Habiba-Adel@users.noreply.github.com>
2026-06-20 23:34:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
 require("dotenv").config();
-
-
 const config = require("./config/env");
-
 const app = require("./app");
-
 const connectDB = require("./config/db");
 
-const PORT = config.port;
+connectDB();
 
-const startServer = async () => {
-  try {
-    await connectDB();
-
-    app.listen(PORT, () => {
-      console.log(
-        `Server running on port ${PORT}`
-      );
-    });
-  } catch (error) {
-    console.error(
-      "Failed to start application:",
-      error
-    );
-
-    process.exit(1);
-  }
-};
-
-startServer();
+module.exports = app;
